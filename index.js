65dba6ddb8bf66b3488112f091456779d3f5e196
Tristan Ludowyk <tristan.ludowyk@gmail.com>
2018-08-05 14:57:32 +0000

    tabs to spaces

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const Gpio = require('pigpio').Gpio;
  
 const createInput = (pin) => new Gpio(pin, {
-	  mode: Gpio.INPUT,
-	  pullUpDown: Gpio.PUD_DOWN,
-	  edge: Gpio.EITHER_EDGE
+  mode: Gpio.INPUT,
+  pullUpDown: Gpio.PUD_DOWN,
+  edge: Gpio.EITHER_EDGE
 });
 
 const motor = new Gpio(18, {mode: Gpio.OUTPUT});
@@ -27,53 +27,53 @@ let timer
 motor.servoWrite(upPulseWidth)
 
 const openGates = () => {
-	clearInterval(timer)
-	timer = setInterval(() => {
-		if (pulseWidth <= upPulseWidth) {
-			clearInterval(timer)
-		} else {
-			pulseWidth -= increment
-  		motor.servoWrite(pulseWidth);
-		}
-	}, interval)
+  clearInterval(timer)
+  timer = setInterval(() => {
+    if (pulseWidth <= upPulseWidth) {
+      clearInterval(timer)
+    } else {
+      pulseWidth -= increment
+      motor.servoWrite(pulseWidth);
+    }
+  }, interval)
 }
  
 const closeGates = () => {
-	clearInterval(timer)
-	timer = setInterval(() => {
-		if (pulseWidth >= downPulseWidth) {
-			clearInterval(timer)
-		} else {
-			pulseWidth += increment
-  		motor.servoWrite(pulseWidth);
-		}
-	}, interval)
+  clearInterval(timer)
+  timer = setInterval(() => {
+    if (pulseWidth >= downPulseWidth) {
+      clearInterval(timer)
+    } else {
+      pulseWidth += increment
+      motor.servoWrite(pulseWidth);
+    }
+  }, interval)
 }
 
 sensor1.on('interrupt', (level) => {
-	console.log(`1 = ${level}: ${state}`)
-	if (!level && (state == TWO_TRIGGERED)) {
-		state = TWO_ONE_TRIGGERED
-	} else if (level && (state == TWO_ONE_TRIGGERED)) {
-		state = NONE
-		openGates()
-	} else if (!level && (state == NONE)) {
-		state = ONE_TRIGGERED
-		closeGates()
-	}
-	console.log(`... ${level}: ${state}`)
+  console.log(`1 = ${level}: ${state}`)
+  if (!level && (state == TWO_TRIGGERED)) {
+    state = TWO_ONE_TRIGGERED
+  } else if (level && (state == TWO_ONE_TRIGGERED)) {
+    state = NONE
+    openGates()
+  } else if (!level && (state == NONE)) {
+    state = ONE_TRIGGERED
+    closeGates()
+  }
+  console.log(`... ${level}: ${state}`)
 })
 
 sensor2.on('interrupt', (level) => {
-	console.log(`2 = ${level}: ${state}`)
-	if (!level && (state == ONE_TRIGGERED)) {
-		state = ONE_TWO_TRIGGERED
-	} else if (level && (state == ONE_TWO_TRIGGERED)) {
-		state = NONE
-		openGates()
-	} else if (!level && (state == NONE)) {
-		state = TWO_TRIGGERED
-		closeGates()
-	}
-	console.log(`... ${level}: ${state}`)
+  console.log(`2 = ${level}: ${state}`)
+  if (!level && (state == ONE_TRIGGERED)) {
+    state = ONE_TWO_TRIGGERED
+  } else if (level && (state == ONE_TWO_TRIGGERED)) {
+    state = NONE
+    openGates()
+  } else if (!level && (state == NONE)) {
+    state = TWO_TRIGGERED
+    closeGates()
+  }
+  console.log(`... ${level}: ${state}`)
 })
